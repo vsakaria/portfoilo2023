791cd1df52d85d9d0d954820971e180c86f8f538
Vishal Sakaria <vsakaria@gmail.com>
2024-07-29 10:19:57 +0100

    Update Certification.js

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -15,8 +15,10 @@ const Certification = () => {
             </div>
           </div>
           <div className="justify-center items-center">
-            <img src="/assets/img/certifications/Managment Mastery.webp" alt="Certification 1" className="mx-auto object-contain h-[50%] w-[50%]" />
-            <img src="/assets/img/certifications/Sales for Engineers.webp" alt="Certification 2" className="mx-auto object-contain h-[50%] w-[50%]" />
+            <img src="/assets/img/certifications/GenAI-cert.webp" alt="Certification" className="mx-auto object-contain h-[50%] w-[50%]" />
+            <img src="/assets/img/certifications/Negotations-cerf.webp" alt="Certification" className="mx-auto object-contain h-[50%] w-[50%]" />
+            <img src="/assets/img/certifications/Managment Mastery.webp" alt="Certification" className="mx-auto object-contain h-[50%] w-[50%]" />
+            <img src="/assets/img/certifications/Sales for Engineers.webp" alt="Certification" className="mx-auto object-contain h-[50%] w-[50%]" />
           </div>
         </div>
       </div>
