e5fd899a43cbafa0afa56022cab449efe020ddc8
Vishal Sakaria <vsakaria@gmail.com>
2025-07-07 15:37:25 +0100

    Update Flat component: remove duplicate image entries and streamline image list for better organization

--- pages/flat/index.js
+++ pages/flat/index.js
@@ -6,6 +6,11 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || '';
 
 const Flat = () => {
   const imageNames = [
+    'b9313e56-b132-4ed2-bd0e-1b6b4c9c9023.jpeg',
+    'b1a36bf6-3462-4f33-99df-35dc4cc3f42b.jpeg',
+    'abd0531a-921c-4f34-ada7-f31a6a6fe474.jpeg',
+    '7793367d-ea40-4328-b7de-b3b07b31814c.jpeg',
+    '988337ab-3da9-40f1-9882-fd29e79920ed.jpeg',
    
     'f43a3e42-dc1e-416c-812d-4a1f07dc40c6.jpeg',
     'edd34c54-97f8-42c5-95ab-b5435894a215.jpeg',
@@ -17,11 +22,7 @@ const Flat = () => {
     'c95cabb6-6282-4b5d-86b1-d64f094a7b8a.jpeg',
     'bdb4ab88-42be-451a-bcc2-8bf073b5fb5c.jpeg',
     'b226153e-56f0-4d5e-8345-b10c7ac10cef.jpeg',
-    'b9313e56-b132-4ed2-bd0e-1b6b4c9c9023.jpeg',
-    'b1a36bf6-3462-4f33-99df-35dc4cc3f42b.jpeg',
-    'abd0531a-921c-4f34-ada7-f31a6a6fe474.jpeg',
-    '7793367d-ea40-4328-b7de-b3b07b31814c.jpeg',
-    '988337ab-3da9-40f1-9882-fd29e79920ed.jpeg',
+    
     '72445b03-4d7d-47b4-bc87-70ffe5ce1bc7.jpeg',
     '60042c88-7756-4f47-826b-b4cb370d80cf.jpeg',
     '3478aaa3-5d90-49c0-ba56-05ad7b870397.jpeg',
